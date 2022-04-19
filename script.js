27bbe695a5ee562e43b8e0e8869a2172a8bc595e
guilhermehjunior <guihjunior7@gmail.com>
2022-04-19 08:08:00 -0300

    final teste

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //Abrir modal, adicionar active ao modal
 // const Modal = document.querySelector('.modal-overlay');
-// testando novamente
+// testando novamente ultimo teste
 // console.log(Modal.classList);
 
 // function open(){
